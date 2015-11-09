2a0b55cc9b6d930a192ba40bbadc7b1645908049
Tanim Islam <tanim.islam@gmail.com>
2015-11-09 08:57:11 -0800

    Changes to be committed: 	modified:   cwMenubar.py

--- cwMenubar.py
+++ cwMenubar.py
@@ -1,6 +1,76 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import cssutils, os, subprocess, cwResources
+import math, numpy
+
+class ColorWheelExpandedColorSwatch( QWidget ):
+    def __init__( self, parent ):
+        super(ColorWheelExpandedColorSwatch, self).__init__( parent )
+        self.parent = parent
+        self.setVisible( False )
+        self.setWindowFlags( Qt.CustomizeWindowHint | Qt.WindowCloseButtonHint | Qt.Window )
+        self.setSizePolicy( QSizePolicy.Fixed, QSizePolicy.Fixed )
+        self.setWindowTitle( 'EXPANDED COLOR SWATCH' )
+        self.prevWidth = -1
+        self.prevHeight = -1
+        #
+        ## hide window action
+        hideAction = QAction( self )
+        hideAction.setShortcut( 'Esc' )
+        hideAction.triggered.connect( self.hideMe )
+        self.addAction( hideAction )
+
+    def hideMe( self ):
+        self.setVisible( False )
+
+    def paintEvent( self, evt ):
+        colorNames = self.parent.cwt.getColorNames( )
+        numCols = int( numpy.sqrt( len( colorNames ) * 1.0 ) ) # candidate number of cols
+        if len( colorNames ) % numCols != 0:
+            numCols += 1
+        numRows, rem = divmod( len( colorNames ), numCols )
+        if rem != 0:
+            numRows += 1
+        print 'GOT HERE IN PAINT!', len( colorNames ), numRows, numCols
+        allHsvs = self.parent.getTransformedHsvs( )
+        painter = QPainter( self )
+        painter.setRenderHint( QPainter.Antialiasing )
+        #
+        ## now set the image
+        width = 170 * numCols
+        height = 170 * numRows
+        if width != self.prevWidth or height != self.prevHeight:
+            print 'GOT HERE!'
+            self.setFixedSize( width, height )
+            image = QImage( width, height, QImage.Format_ARGB32 )
+            image.fill( QColor( "white" ).rgb( ) )
+            painter.drawImage( 0, 0, image )
+            self.prevWidth = width
+            self.prevHeight = height
+        #
+        ## now do your plotting
+        color = QColor( )
+        brush = QBrush( )
+        for idx, tup in enumerate( allHsvs ):
+            row, col = divmod( idx, numCols )
+            h, s, v = tup
+            color.setHsvF( h, s, v, 1.0 )
+            brush.setColor( color )
+            painter.setBrush( brush )
+            painter.drawRect( 10 + 170 * row, 10 + 170 * col, 150, 150 )
+        font = QFont( )
+        font.setFamily( 'Alef' )
+        font.setPointSize( 12 )
+        font.setBold( True )
+        painter.setFont( font )
+        pen = QPen( )
+        pen.setColor( QColor( "black" ) )
+        painter.setPen( pen )
+        for idx, colorname in enumerate( colorNames ):
+            row, col = divmod( idx, numCols )
+            painter.drawText( QRect( 10 + 170 * row, 10 + 170 * col, 150, 150 ),
+                              Qt.AlignCenter, colorname )
+        
 
 class ColorWheelMenuBar( QMenuBar ):
     def __init__(self, parent ):
@@ -14,15 +84,20 @@ class ColorWheelMenuBar( QMenuBar ):
         #
         opsMenu = self.addMenu( '&Operations' )
         showExpandedColorSwatchAction = opsMenu.addAction('&Expanded Color Swatch' )
+        self._expandedColorSwatch = ColorWheelExpandedColorSwatch( parent )
         #
         helpMenu = self.addAction( '&Help' )
+        aboutMenu = self.addAction( '&About' )
         #
         ## actions
         self.saveAction.setShortcut('Shift+Ctrl+S')
         self.saveAction.triggered.connect( self.saveCSSFile )
         openAction.setShortcut('Shift+Ctrl+O')
         openAction.triggered.connect( self.openCSSFile )
+        openURLAction.setShortcut('Shift+Ctrl+U')
         openURLAction.triggered.connect( self.openCSSURLFile )
+        showExpandedColorSwatchAction.triggered.connect( self.expandColorSwatchAction )
+        
 
     def enableSaveAction( self ):
         self.saveAction.setEnabled( True )
@@ -30,6 +105,11 @@ class ColorWheelMenuBar( QMenuBar ):
     def disableSaveAction( self ):
         self.saveAction.setEnabled( False )
 
+    def expandColorSwatchAction( self ):
+        if len(self.parent.cwt.getColorNames( )) <= 1:
+            return
+        if not self._expandedColorSwatch.isVisible( ):
+            self._expandedColorSwatch.setVisible( True )
     def openCSSURLFile( self ):
         while(True):
             myURL, ok = QInputDialog.getText( self, 'Open CSS URL. Can end by pressing cancel or by putting in blank.',
