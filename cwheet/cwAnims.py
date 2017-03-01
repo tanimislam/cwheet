4e0fe899f96ca007f367fcdc6c365718a7e633b9
Tanim <tanim.islam@gmail.com>
2017-03-01 09:43:37 -0800

    Had a __main__ before in cwAnims.py, moved out to cwAnimsExec.py. Also modified the requirements.txt
    Changes to be committed:
    	modified:   cwAnimsExec.py
    	modified:   cwheet/cwAnims.py

--- cwheet/cwAnims.py
+++ cwheet/cwAnims.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python2
-
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 from cwAll import ColorWheelAll
@@ -542,31 +540,3 @@ class OperationSliderAnimation( QWidget ):
         self.runGoRun.partDone.connect( self._updateColorWheel )
         self.runGoRun.finished.connect( self._createMovieAndStop )
         self.runGoRun.start( )
-        
-if __name__=='__main__':
-    parser = OptionParser()
-    parser.add_option('--dohue', dest='do_hue', action='store_true', default = False,
-                      help = 'If chosen, do a HUE transform movie' )
-    parser.add_option('--dosat', dest='do_sat', action='store_true', default = False,
-                      help = 'If chosen, do a SATURATION transform movie' )
-    parser.add_option('--doval', dest='do_val', action='store_true', default = False,
-                      help = 'If chosen, do a VALUE transform movie' )
-    parser.add_option('--debug', dest='do_debug_logging', action='store_true', default = False,
-                      help = 'If chosen, do debug logging.')
-    opts, args = parser.parse_args()
-    if len(filter( lambda tok: tok is True, ( opts.do_hue, opts.do_sat, opts.do_val ) ) ) != 1:
-        parser.parse_args(['-h'])
-    if opts.do_debug_logging:
-        logfile =  os.path.join( os.path.expanduser('~/temp'), 'debug.log' )                                 
-        logging.basicConfig( filename = logfile, level = logging.DEBUG )
-    #
-    ##
-    app = QApplication([])
-    if opts.do_hue:
-        osa = OperationSliderAnimation( OperationAnimation.HUETRANSFORM )
-    elif opts.do_sat:
-        osa = OperationSliderAnimation( OperationAnimation.SATURATIONTRANSFORM )
-    else:
-        osa = OperationSliderAnimation( OperationAnimation.VALUETRANSFORM )
-    osa.show( )
-    sys.exit( app.exec_( ) )
